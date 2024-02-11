bbd4a6fd295afb85b1b33d595368d9ef681b3277
Moritz André Myrseth <moritzmyrz@gmail.com>
2024-02-11 05:29:37 +0100

    Add CreditNotes class with methods

---
+++ src/creditNotes/index.ts
@@ -0,0 +1,33 @@
+import { Base } from '../base';
+
+export class CreditNotes extends Base {
+	getCreditNotes() {}
+
+	createFullCreditNote() {}
+
+	createPartialCreditNote() {}
+
+	getCreditNote() {}
+
+	sendCreditNote() {}
+
+	getCreditNoteCounter() {}
+
+	createCreditNoteCounter() {}
+
+	getCreditNoteDrafts() {}
+
+	createCreditNoteDraft() {}
+
+	getCreditNoteDraft() {}
+
+	updateCreditNoteDraft() {}
+
+	deleteCreditNoteDraft() {}
+
+	getCreditNoteDraftAttachments() {}
+
+	createCreditNoteDraftAttachment() {}
+
+	createCreditNoteFromDraft() {}
+}
