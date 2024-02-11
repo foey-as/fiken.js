b8aea47a8b0a5b6f892e9d9cfe95cff384a916a1
Moritz André Myrseth <moritzmyrz@gmail.com>
2024-02-11 04:58:10 +0100

    Refactor getAccounts method to include query parameters

--- src/accounts/index.ts
+++ src/accounts/index.ts
@@ -1,14 +1,17 @@
 import { Base } from '../base';
-import { Account } from './types';
+import { Account, AccountsParams } from './types';
 
 const resourceName = 'accounts';
 
 export class Accounts extends Base {
-	getAccounts() {
-		return this.request<Account[]>('accounts');
+	getAccounts(params?: AccountsParams) {
+		const searchParams = this.prepareParamsForURLSearch(params);
+		const queryString = `?${new URLSearchParams(searchParams).toString()}`;
+
+		return this.request<Account[]>(resourceName + queryString);
 	}
 
 	getAccount(accountCode: string) {
-		return this.request<Account>(`accounts/${accountCode}`);
+		return this.request<Account>(`${resourceName}/${accountCode}`);
 	}
 }
