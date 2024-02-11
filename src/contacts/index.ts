9511e41895fe88c00574c7f9c87d9f8209e43e95
Moritz André Myrseth <moritzmyrz@gmail.com>
2024-02-11 05:19:09 +0100

    Add contact and contact person CRUD operations

--- src/contacts/index.ts
+++ src/contacts/index.ts
@@ -10,15 +10,27 @@ export class Contacts extends Base {
 		return this.request<Contact>(`/contacts/${contactId}`);
 	}
 
+	createContact() {}
+
+	updateContact() {}
+
 	getContactPersons(contactId: number) {
 		return this.request<ContactPerson[]>(
 			`/contacts/${contactId}/contactPerson`
 		);
 	}
 
+	createContactPersonAttachment() {}
+
 	getContactPerson(contactId: number, contactPersonId: number) {
 		return this.request<ContactPerson>(
 			`/contacts/${contactId}/contactPerson/${contactPersonId}`
 		);
 	}
+
+	createContactPerson() {}
+
+	updateContactPerson() {}
+
+	deleteContactPerson() {}
 }
